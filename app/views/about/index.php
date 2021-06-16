4e86a6395efb3015e6af7f866683132f4842359e
Nabila Syafa'ah <71918131+nabilasyafaah@users.noreply.github.com>
2021-06-16 12:04:00 +0700

    Update About Us Page

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,1 +1,128 @@
-<h1>View pada about</h1>
+<div class="container costum">
+  <div class="row align-items-center justify-content-center">
+    <div class="col-sm-12 col-md-5">
+      <h1>About Us</h1>
+      <p>
+      Tujuan utama kami adalah untuk mewujudkan masyarakat yang berakhlak mulia, bermoral, 
+      beretika, berbudaya, dan beradab berdasarkan falsafah Pancasila. Harapan kami membimbing lebih 
+      banyak orang untuk ikut berupaya dalam mewujudkan bangsa yang berdaya-saing. Kami yakin bahwa 
+      Indonesia mampu memiliki masyarakat yang berjiwa demokratis berlandaskan hukum. Impian kami yang
+      utama adalah menciptakan Indonesia aman, damai, dan bersatu. mewujudkan pemerataan pembangunan dan 
+      berkeadilan. Serta mewujudkan Indonesia asri dan lestari yang mampu berperan penting dalam pergaulan dunia 
+      internasional. 
+      </p>
+      <button type="button" class="btn btn-outline-success btn-lg">
+        Learn more
+      </button>
+      <button type="button" class="btn btn-outline-warning btn-lg">
+        Support us
+      </button>
+    </div>
+    <div class="col-sm-12 col-md-5 offset-1 img-responsive">
+      <img
+        src="img/About Us.jpg"
+        alt="About Us Image"
+      />
+  </div>
+</div>
+<div class="container">
+  <div class="row justify-content-center p-5">
+    <div class="col-sm-8">
+      <div class="p-12 bg-light">
+      “If poverty is a disease that infects the entire community in the form of unemployment and violence, failing schools and broken homes, 
+      then we can’t just treat those symptoms in isolation. We have to heal that entire community.” — Barack Obama
+      <br>
+      (Jika kemiskinan adalah penyakit yang menginfeksi seluruh komunitas dalam bentuk pengangguran dan kekerasan, pendidikan yang gagal, 
+      dan rumah tangga yang berantakan, maka kita tidak bisa begitu saja mengobati gejala-gejala itu secara terpisah. Kita harus menyembuhkan seluruhnya.)
+      </div>
+    </div>
+  </div>
+</div>
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-md-5">
+          <button type="button" class="btn btn-outline-warning mb-3">
+          Hubungi Kami Sekarang! 
+          </button>
+          <h1>Layanan Donatur Kami</h1>
+          <p>
+            Kami memberikan kemudahan bagi anda yang ingin ikut berkontribusi menuju kebaikan
+            bersama Indonesia. Donasi anda akan disalurkan kepada pihak paling membutuhkan.
+            <br>
+            Contact Person:
+            <br>
+            Sheryl Aurelia (+62 823-0107-7262)
+            <br>
+            Vida Komaria (+62 822-5760-0224)
+            <br>
+            Nabila Syafa'ah Shoufia Dalillah (+62 895-1316-1216)
+          </p>
+        </div>
+        <div class="col-md-5"></div>
+        <div class="col-md-5"></div>
+        <div class="col-md-5">
+          <button type="button " class="btn btn-outline-success mb-3">
+          Berdonasi Sekarang!
+          </button>
+          <h1>Jam Operasional</h1>
+          <p>
+            Kami siap melayani anda setiap hari Senin - Jumat tepatnya pada pukul 09.00 – 16.00
+            pada Waktu Indonesia Barat.
+          </p>
+        </div>
+        <div class="col-md-5">
+          <button type="button " class="btn btn-outline-success mb-3">
+          Berdonasi Sekarang!
+          </button>
+          <h1>We LALALA.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Delectus, tempore placeat corrupti enim, cumque ex? Mollitia nihil
+            sint cumque omnis iure nisi.
+          </p>
+        </div>
+        <div class="col-md-5"><img src="img/revenue_.svg" alt="" /></div>
+      </div>
+    </div>
+  
+    </section>
+<section class="contact p-5">
+      <div class="container">
+        <div class="row  align-items-center justify-content-center">
+          <div class="col-md-5">
+            <h1>Contact us:</h1>
+            <div class="mb-3">
+              <label 
+                for="exampleFormControlInput1" 
+                class="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+            <div class="mb-3">
+              <label
+                for="exampleFormControlTextarea1" 
+                class="form-label">
+              Pesan untuk kami
+              </label>
+              <textarea
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+            <button type="button" class="btn btn-outline-secondary">
+              Send
+            </button>
+          </div>
+          <div class="col-md-5">
+            <img src="img/handshake.svg" alt="Contact image" />
+          </div>
+        </div>
+      </div>
+    </section>
