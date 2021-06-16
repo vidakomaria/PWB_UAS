e804a948369a4a61732b25a7b8f95e11ebf4ad00
Nabila Syafa'ah <71918131+nabilasyafaah@users.noreply.github.com>
2021-06-16 17:35:51 +0700

    Update About.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -2,8 +2,8 @@
 
 class About extends Controller{
     public function index(){
-        $data['judul']='About';
-        $data['akun']=$this->model('Akun_model')->getDataAkun();
+//        $data['judul']='About';
+//        $data['akun']=$this->model('Akun_model')->getDataAkun();
         $this->view('templates/header',$data);
         $this->view('about/index',$data);
         $this->view('templates/footer');
