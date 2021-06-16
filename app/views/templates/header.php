dbbebad572bda0208788e4c256c34aa1b5b01479
Nabila Syafa'ah <71918131+nabilasyafaah@users.noreply.github.com>
2021-06-16 17:09:20 +0700

    Ganti "Judul" di header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -11,7 +11,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light justify-content-end" style="background-color: #03256C  ">
     <div class="container">
-        <a class="navbar-brand text-capitalize text-white " href="<?=BASEURL;?>">"Judul"</a>
+        <a class="navbar-brand text-capitalize text-white " href="<?=BASEURL;?>">WeCareIndo</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
